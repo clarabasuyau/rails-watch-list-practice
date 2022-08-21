fc191c250f90174aa97944e22bf47d576b79b189
clara basuyau <clarabasuyau@gmail.com>
2022-08-21 20:25:34 +0200

    step3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :lists, only: %I[new show create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "open-uri"
+
+puts "Cleaning up db..."
+Movie.destroy_all
+puts "Database cleaned"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)['results']
+
+movies.each do |movie|
+  puts "Creating #{movie['title']}"
+  base_poster_url = "https://image.tmdb.org/t/p/original"
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts 'Movies created'
